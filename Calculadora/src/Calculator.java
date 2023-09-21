a72f0b6a6f251ba506c01cbf3e8e2cc7e22ed0fd
jorgekalas <jorgekalas@gmail.com>
2023-09-20 21:26:10 -0300

    multiplication and division methods

--- Calculadora/src/Calculator.java
+++ Calculadora/src/Calculator.java
@@ -1,2 +1,13 @@
 public class Calculator {
+
+    public static int multiply (int a, int b){
+        return a * b;
+    }
+
+    public static double divide (int a, int b){
+        if (b == 0){
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return (double) a / b;
+    }
 }
